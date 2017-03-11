b9086a089db8f4930b3565b3c61bfafa9e7749b8
igprad <ign.aldi.pradana@gmail.com>
2017-03-11 15:51:47 +0700

    Added init FpkController

---
+++ src/main/java/com/empatkepala/controller/FpkController.java
@@ -0,0 +1,10 @@
+package com.empatkepala.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ALz on 3/11/2017.
+ */
+@RestController(value="/fpk")
+public class FpkController {
+}
