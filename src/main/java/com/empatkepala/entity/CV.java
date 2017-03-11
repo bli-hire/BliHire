16e5bb31e0b1664155eb42820ebcbd1f509c1aa1
Ryan Bagus Susilo <ryan2009bs@gmail.com>
2017-03-11 18:19:05 +0700

    Add CV Class exp

---
+++ src/main/java/com/empatkepala/entity/CV.java
@@ -0,0 +1,7 @@
+package com.empatkepala.entity.request;
+
+/**
+ * Created by Ryan Bagus Susilo on 3/11/2017.
+ */
+public class CV {
+}
---
+++ src/main/java/com/empatkepala/entity/CVEntity/WorkExperience.java
@@ -0,0 +1,7 @@
+package com.empatkepala.entity.CVEntity;
+
+/**
+ * Created by Ryan Bagus Susilo on 3/11/2017.
+ */
+public class WorkExperience {
+}
