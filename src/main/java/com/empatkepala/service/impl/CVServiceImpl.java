dcf4a223dd534b546cf83fffddb481d69a1e97f7
Ryan Bagus Susilo <ryan2009bs@gmail.com>
2017-07-30 16:41:42 +0700

    merapihkan Service Impl

--- src/main/java/com/empatkepala/service/impl/CVServiceImpl.java
+++ src/main/java/com/empatkepala/service/impl/CVServiceImpl.java
@@ -16,23 +16,10 @@ public class CVServiceImpl implements CVService{
     @Autowired
     CVRepository cvRepository;
 
-
-
-//    public User getUserByRequestedBy(User user){
-//
-//        Mpp mpp = mppRepository.findOne(1L);
-//        Hibernate.initialize(mpp.getRequestedBy());
-//        return mpp.getRequestedBy();
-//    }
-
-
     public CV getRequestedByCVId(Long id){
-
         CV cv = cvRepository.findOne(id);
-//        Hibernate.initialize(mpp.getRequestedBy());
         return cv.getRequestedBy();
     }
-
 
     public CV getCVById(Long id){
         return cvRepository.findOne(id);
