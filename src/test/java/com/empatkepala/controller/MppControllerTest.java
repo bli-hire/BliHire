9cd08729abc8ff14c7fd5c3e04eec9a9ff2bff1e
Ahmad Widardi <awidardi@gmail.com>
2017-08-10 04:47:41 +0700

    Added Mpp controller unit test

---
+++ src/test/java/com/empatkepala/controller/MppControllerTest.java
@@ -0,0 +1,69 @@
+package com.empatkepala.controller;
+
+
+import com.empatkepala.entity.Mpp;
+import com.empatkepala.entity.MppDetail;
+import com.empatkepala.entity.User;
+import com.empatkepala.entity.request.AddMppRequest;
+import com.empatkepala.enumeration.Department;
+import com.empatkepala.enumeration.Role;
+import com.empatkepala.service.JobVacancyService;
+import com.empatkepala.service.MppService;
+import com.empatkepala.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class MppControllerTest {
+
+    @MockBean
+    private MppService mppService;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private JobVacancyService jobVacancyService;
+
+    @LocalServerPort
+    private int serverPort;
+
+    private static final Role ROLE = Role.CEO;
+    private static final String NAME = "Dummy";
+    private static final String SURNAME = "Account";
+    private static final String PASSWORD = "dummy";
+    private static final String EMAIL = "dummy@account.com";
+
+    private static final Department DEPARTMENT = Department.Finance;
+
+    @Test
+    public void findAllMpp()
+    {
+        when(mppService.getAllMpp()).thenReturn(Arrays.asList(new Mpp(new User(ROLE, DEPARTMENT, NAME, SURNAME, PASSWORD, EMAIL), DEPARTMENT)));
+
+        given()
+                .contentType("application/json")
+                .when()
+                .port(serverPort)
+                .get("/mpp")
+                .then()
+                .body(containsString(DEPARTMENT.name()))
+                .statusCode(200);
+
+        verify(mppService).getAllMpp();
+    }
+
+
+}
