ae6b82c0fb352de904d965eb35148051f876a819
Ryan Bagus Susilo <ryan2009bs@gmail.com>
2017-08-20 15:14:15 +0700

    tambah method di controller untuk upload file dan tambah configurasi di properties

--- src/main/java/com/empatkepala/controller/CVController.java
+++ src/main/java/com/empatkepala/controller/CVController.java
@@ -10,12 +10,19 @@ import com.empatkepala.service.CVService;
 import com.empatkepala.service.UserService;
 import com.empatkepala.view.MyPdfView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -28,6 +35,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CVController {
     @Autowired
     private CVService cvService;
+
+    @Autowired
+    private Environment env;
 
 
     @RequestMapping(method = RequestMethod.GET,produces = "application/json")
@@ -94,4 +104,29 @@ public class CVController {
         model.put("cv", cvCollection);
         return new ModelAndView(new MyPdfView(), model);
     }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> uploadFile(
+            @RequestParam("uploadfile") MultipartFile uploadfile) {
+
+        try {
+            // Get the filename and build the local file path
+            String filename = uploadfile.getOriginalFilename();
+            String directory = env.getProperty("netgloo.paths.uploadedFiles");
+            String filepath = Paths.get(directory, filename).toString();
+
+            // Save the file locally
+            BufferedOutputStream stream =
+                    new BufferedOutputStream(new FileOutputStream(new File(filepath)));
+            stream.write(uploadfile.getBytes());
+            stream.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    } // method uploadFile
 }
